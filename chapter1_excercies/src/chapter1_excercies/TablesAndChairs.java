4838d526b61d8c2ac63c13db4207bd20133ed4bd
De'Antwan Hanes <Deantwanhanes@gmail.com>
2018-08-27 09:50:30 -0500

    h

---
+++ chapter1_excercies/src/chapter1_excercies/TablesAndChairs.java
@@ -0,0 +1,27 @@
+package chapter1_excercies;
+
+public class TablesAndChairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+		
+		System.out.println("X                X");
+		System.out.println("X                X");
+		System.out.println("X                X");
+		System.out.println("X  XXXXXXX       X");
+		System.out.println("X X            X X");
+		System.out.println("XXXXX X    X XXXXX");
+	}
+
+	public static void displayTruck() {
+		
+		System.out.println("X   XXXXXXXXX X");
+		System.out.println("X X X  XX X XXX");
+		System.out.println("XXXXXXX XXXXXXX");
+		System.out.println("XXX XXXX XXXXXX");
+		System.out.println("XXXXXXXXXXXXXXX");
+	}
+}
